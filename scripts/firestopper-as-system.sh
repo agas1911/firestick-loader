2661266a57f42c127d16f42fde9b397a084fbb89
Jason Borowski <esc0rtd3w@gmail.com>
2016-08-21 07:48:52 -0400

    Fixed Typo

--- scripts/firestopper-as-system.sh
+++ scripts/firestopper-as-system.sh
@@ -7,7 +7,7 @@ chmod 0755 /system/app/FireStopper/
 
 cp /data/local/tmp/firestopper.apk /system/app/FireStopper/FireStopper.apk
 
-#chown 0.0 /system/priv-app/FireStopper/FireStopper.apk
+#chown 0.0 /system/app/FireStopper/FireStopper.apk
 chmod 0644 /system/app/FireStopper/FireStopper.apk
 
 am start -a de.belu.firestopper/.gui.MainActivity -n de.belu.firestopper/.gui.MainActivity
