77b82515b117a900e5d291372de4299542d7e58b
Jason Borowski <esc0rtd3w@gmail.com>
2016-09-04 19:04:28 -0400

    Updated Scripting To Clean Crash Dump Files

--- scripts/kodi/windows/prep-for-packing-rebuild.cmd
+++ scripts/kodi/windows/prep-for-packing-rebuild.cmd
@@ -8,6 +8,8 @@ title Prep Kodi For Packaging                             esc0rtd3w 2016
 
 del /f /q "%AppData%\Kodi\kodi.log"
 del /f /q "%AppData%\Kodi\kodi.old.log"
+del /f /q "%AppData%\Kodi\kodi_crashlog-*.dmp"
+del /f /q "%AppData%\Kodi\kodi_stacktrace-*.txt"
 del /f /q "%AppData%\Kodi\addons\packages\*"
 rd /s /q "%AppData%\Kodi\cache"
 rd /s /q "%AppData%\Kodi\media"
