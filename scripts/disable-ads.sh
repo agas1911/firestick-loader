09ce3e175870f2dc5e28f0eacaacd3b3dbacd12b
Jason Borowski <esc0rtd3w@gmail.com>
2016-09-04 05:45:27 -0400

    Updated Permissions To Allow Non-Root Adblock Updates

--- scripts/disable-ads.sh
+++ scripts/disable-ads.sh
@@ -11,6 +11,7 @@ rm /system/etc/hosts /system/etc/hosts
 mv /data/local/tmp/hosts /system/etc/hosts
 
 chown 0.0 /system/etc/hosts
-chmod 0644 /system/etc/hosts
+#chmod 0644 /system/etc/hosts
+chmod 777 /system/etc/hosts
 
 
