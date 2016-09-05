42e9b43af8bd5c27a3cd9a0a8a7287715ab078de
Jason Borowski <esc0rtd3w@gmail.com>
2016-09-05 03:10:44 -0400

    Updated Permissions

--- scripts/install-stock-lollipop-5.1-settings.sh
+++ scripts/install-stock-lollipop-5.1-settings.sh
@@ -5,9 +5,9 @@ name="Settings"
 mount -o rw,remount /system
 
 mkdir /system/app/$name/
-chmod 0755 /system/app/$name/
+chmod 755 /system/app/$name/
 
 cp /data/local/tmp/settings-5.1.apk /system/app/$name/$name.apk
 
 chown 0.0 /system/app/$name/$name.apk
-chmod 0644 /system/app/$name/$name.apk
+chmod 644 /system/app/$name/$name.apk
