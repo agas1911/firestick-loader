4d89daa55ceb4a1de85bcbd7707ef57bf6f7c250
Jason Borowski <esc0rtd3w@gmail.com>
2016-09-05 21:22:09 -0400

    Updated Scripting

--- scripts/misc/launch-activity.cmd
+++ scripts/misc/launch-activity.cmd
@@ -3,11 +3,13 @@
 color 0b
 
 set adb="..\..\bin\adb.exe"
+set _color="..\..\bin\cocolor.exe"
 
 
 
 
 :loop
+%_color% 0b
 
 cls
 echo Enter Activity and Press Enter:
@@ -18,9 +20,17 @@ echo.
 echo.
 echo.
 
+%_color% 0e
+
 set /p activity=
 
 %adb% shell "am start -a android.intent.action.MAIN -n %activity%"
+
+%_color% 0b
+
+echo.
+echo.
+pause
 
 goto loop
 
