92b608a0a80f02ecef36f7433f03607d01b2f747
Jason Borowski <esc0rtd3w@gmail.com>
2016-09-10 01:37:38 -0400

    Updated Script

--- scripts/misc/update-protection-loop.cmd
+++ scripts/misc/update-protection-loop.cmd
@@ -66,6 +66,12 @@ echo.
 echo Trying ROOT Commands....
 echo.
 echo.
+echo.
+echo Attempting To Clear Cache and Remove OTA BIN Files....
+echo.
+echo.
+echo.
+echo.
 
 :: Request SU Permission
 %shell% "su"
@@ -94,6 +100,25 @@ echo.
 %shell% "su -c chmod 777 /cache/"
 %shell% "su -c chmod 777 /cache/recovery/"
 
+echo.
+echo.
+echo.
+echo Attempting To Disable Amazon OTA Updates....
+echo.
+echo.
+
+%shell% "su -c pm hide com.amazon.dcp"
+%shell% "su -c pm hide com.amazon.settings.systemupdates/.OTAEventReceiver"
+%shell% "su -c pm hide com.amazon.device.software.ota"
+%shell% "su -c pm hide com.amazon.device.software.ota.override"
+%shell% "su -c pm hide com.amazon.tv.nimh"
+
+%shell% "su -c pm disable com.amazon.dcp"
+%shell% "su -c pm disable com.amazon.settings.systemupdates/.OTAEventReceiver"
+%shell% "su -c pm disable com.amazon.device.software.ota"
+%shell% "su -c pm disable com.amazon.device.software.ota.override"
+%shell% "su -c pm disable com.amazon.tv.nimh"
+
 %sleep% 1
 
 
@@ -103,6 +128,10 @@ echo.
 echo.
 echo.
 echo Trying USER Commands....
+echo.
+echo.
+echo.
+echo Attempting To Clear Cache and Remove OTA BIN Files....
 echo.
 echo.
 %shell% "rm -f /cache/*.bin"
@@ -124,6 +153,24 @@ echo.
 
 %shell% "chmod 777 /cache/"
 %shell% "chmod 777 /cache/recovery/"
+
+echo.
+echo.
+echo.
+echo Attempting To Disable Amazon OTA Updates....
+echo.
+echo.
+%shell% "pm hide com.amazon.dcp"
+%shell% "pm hide com.amazon.settings.systemupdates/.OTAEventReceiver"
+%shell% "pm hide com.amazon.device.software.ota"
+%shell% "pm hide com.amazon.device.software.ota.override"
+%shell% "pm hide com.amazon.tv.nimh"
+
+%shell% "pm disable com.amazon.dcp"
+%shell% "pm disable com.amazon.settings.systemupdates/.OTAEventReceiver"
+%shell% "pm disable com.amazon.device.software.ota"
+%shell% "pm disable com.amazon.device.software.ota.override"
+%shell% "pm disable com.amazon.tv.nimh"
 
 %sleep% 1
 
