c1290e0b595197426039c94f8606413a586d01a0
Jason Borowski <esc0rtd3w@gmail.com>
2016-09-10 01:53:49 -0400

    Updated Update Protection Scripts

--- scripts/misc/update-protection-loop-standalone.cmd
+++ scripts/misc/update-protection-loop-standalone.cmd
@@ -8,6 +8,49 @@ title Update Protection Loop Script [Try Preventing FireOS 5.2.1.1+ Update]  [es
 
 color 0e
 
+set runShellNoTerminateAndWait=cmd /k
+set runShellNoTerminate=start cmd /k
+set runShellWaitNoTerminate=start /wait cmd /k
+set runShellTerminateAndWait=cmd /c
+set runShellTerminate=start cmd /c
+set runShellWaitTerminate=start /wait cmd /c
+
+set _color="..\..\bin\cocolor.exe"
+set sleep="..\..\bin\wait.exe"
+set rm=rmdir /s /q
+
+set adb="..\..\bin\adb.exe"
+set adbKill="%adb% kill-server
+set adbStart="%adb% start-server
+set adbWait=%adb% wait-for-device
+
+set appName=0
+set choice=2
+
+set install=%adb% install
+set uninstall=%adb% uninstall
+set push=%adb% push
+set pull=%adb% pull
+set shell=%adb% shell
+
+set tap=%adb% shell input tap
+set swipe=%shell% input swipe
+set key=%shell% input keyevent
+
+set swipeUp=%shell% input swipe 200 900 200 300
+set keyEnter=%shell% input keyevent 66
+set keyOk=%shell% input keyevent 23
+set keyTab=%shell% input keyevent 61
+set keyArrowUp=%shell% input keyevent 19
+set keyArrowDown=%shell% input keyevent 20
+set keyArrowLeft=%shell% input keyevent 21
+set keyArrowRight=%shell% input keyevent 22
+
+set keyBack=%shell% input keyevent 3
+set keyHome=%shell% input keyevent 4
+
+set amStart=%shell% am start -a android.intent.action.MAIN -n
+
 
 
 
