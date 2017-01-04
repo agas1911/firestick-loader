656268987e4d5d04c7d9e18fd64583db7c90e123
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-04 03:02:04 -0500

    Added TWRP for Montoya

--- recovery/stick/read_recovery.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-
-adb shell dd if=/dev/block/platform/sdhci.1/by-name/recovery of=/sdcard/recovery.img
--- recovery/stick/write_recovery.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-pause
-
-adb shell dd if=/sdcard/recovery.img of=/dev/block/platform/sdhci.1/by-name/recovery
