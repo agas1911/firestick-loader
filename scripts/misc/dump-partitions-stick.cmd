2c05967a3e1621334ce8e48e9c868b23b25c9f4b
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-16 21:57:27 -0500

    Updated Text and Removed cache and userdata From Default Dump

--- scripts/misc/dump-partitions-stick.cmd
+++ scripts/misc/dump-partitions-stick.cmd
@@ -23,6 +23,10 @@ set shell=%adb% shell
 
 set dd=%shell% "su -c dd
 
+set blockBase=/dev/block/platform/sdhci.1/by-name
+
+set dumpFolder="%temp%\firestick-loader\partitions-dump"
+
 set swipeUp=%shell% input swipe 200 900 200 300
 set keyEnter=%shell% input keyevent 66
 set keyTab=%shell% input keyevent 61
@@ -33,91 +37,219 @@ set sdcard=sdcard
 ::set sdcard=extSdCard
 
 
-if exist "%temp%\firestick-loader\partitions-dump" rd /q /s "%temp%\firestick-loader\partitions-dump"
-mkdir "%temp%\firestick-loader\partitions-dump"
+if exist %dumpFolder% rd /q /s %dumpFolder%
+mkdir %dumpFolder%
 
-cls
-echo Dumping Partitions....
+echo.
+echo.
+echo.
+echo Dumping boot Partition....
 echo.
 echo.
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/boot of=/sdcard/boot.img"
-%pull% %sdcard%/boot.img "%temp%\firestick-loader\partitions-dump"
+%dd% if=%blockBase%/boot of=/%sdcard%/boot.img"
+%pull% %sdcard%/boot.img %dumpFolder%
 %shell% rm %sdcard%/boot.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/cache of=/sdcard/cache.img"
-%pull% %sdcard%/cache.img "%temp%\firestick-loader\partitions-dump"
-%shell% rm %sdcard%/cache.img
+echo.
+echo.
+echo.
+echo Dumping cache Partition....
+echo.
+echo.
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/custom-cert of=/sdcard/custom-cert.img"
-%pull% %sdcard%/custom-cert.img "%temp%\firestick-loader\partitions-dump"
+::%dd% if=%blockBase%/cache of=/%sdcard%/cache.img"
+::%pull% %sdcard%/cache.img %dumpFolder%
+::%shell% rm %sdcard%/cache.img
+
+echo.
+echo.
+echo.
+echo Dumping custom-cert Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/custom-cert of=/%sdcard%/custom-cert.img"
+%pull% %sdcard%/custom-cert.img %dumpFolder%
 %shell% rm %sdcard%/custom-cert.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/diag of=/sdcard/diag.img"
-%pull% %sdcard%/diag.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping diag Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/diag of=/%sdcard%/diag.img"
+%pull% %sdcard%/diag.img %dumpFolder%
 %shell% rm %sdcard%/diag.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/dt-blob of=/sdcard/dt-blob.img"
-%pull% %sdcard%/dt-blob.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping dt-blob Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/dt-blob of=/%sdcard%/dt-blob.img"
+%pull% %sdcard%/dt-blob.img %dumpFolder%
 %shell% rm %sdcard%/dt-blob.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/fbi of=/sdcard/fbi.img"
-%pull% %sdcard%/fbi.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping fbi Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/fbi of=/%sdcard%/fbi.img"
+%pull% %sdcard%/fbi.img %dumpFolder%
 %shell% rm %sdcard%/fbi.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/goldpersist of=/sdcard/goldpersist.img"
-%pull% %sdcard%/goldpersist.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping goldpersist Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/goldpersist of=/%sdcard%/goldpersist.img"
+%pull% %sdcard%/goldpersist.img %dumpFolder%
 %shell% rm %sdcard%/goldpersist.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/gpt of=/sdcard/gpt.img"
-%pull% %sdcard%/gpt.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping gpt Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/gpt of=/%sdcard%/gpt.img"
+%pull% %sdcard%/gpt.img %dumpFolder%
 %shell% rm %sdcard%/gpt.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/loader of=/sdcard/loader.img"
-%pull% %sdcard%/loader.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping loader Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/loader of=/%sdcard%/loader.img"
+%pull% %sdcard%/loader.img %dumpFolder%
 %shell% rm %sdcard%/loader.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/misc of=/sdcard/misc.img"
-%pull% %sdcard%/misc.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping misc Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/misc of=/%sdcard%/misc.img"
+%pull% %sdcard%/misc.img %dumpFolder%
 %shell% rm %sdcard%/misc.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/mobicore of=/sdcard/mobicore.img"
-%pull% %sdcard%/mobicore.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping mobicore Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/mobicore of=/%sdcard%/mobicore.img"
+%pull% %sdcard%/mobicore.img %dumpFolder%
 %shell% rm %sdcard%/mobicore.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/persist of=/sdcard/persist.img"
-%pull% %sdcard%/persist.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping persist Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/persist of=/%sdcard%/persist.img"
+%pull% %sdcard%/persist.img %dumpFolder%
 %shell% rm %sdcard%/persist.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/recovery of=/sdcard/recovery.img"
-%pull% %sdcard%/recovery.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping recovery Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/recovery of=/%sdcard%/recovery.img"
+%pull% %sdcard%/recovery.img %dumpFolder%
 %shell% rm %sdcard%/recovery.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/system of=/sdcard/system.img"
-%pull% %sdcard%/system.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping system Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/system of=/%sdcard%/system.img"
+%pull% %sdcard%/system.img %dumpFolder%
 %shell% rm %sdcard%/system.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/u-boot of=/sdcard/u-boot.img"
-%pull% %sdcard%/u-boot.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping u-boot Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/u-boot of=/%sdcard%/u-boot.img"
+%pull% %sdcard%/u-boot.img %dumpFolder%
 %shell% rm %sdcard%/u-boot.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/u-boot-env of=/sdcard/u-boot-env.img"
-%pull% %sdcard%/u-boot-env.img "%temp%\firestick-loader\partitions-dump"
+echo.
+echo.
+echo.
+echo Dumping u-boot-env Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/u-boot-env of=/%sdcard%/u-boot-env.img"
+%pull% %sdcard%/u-boot-env.img %dumpFolder%
 %shell% rm %sdcard%/u-boot-env.img
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/userdata of=/sdcard/userdata.img"
-%pull% %sdcard%/userdata.img "%temp%\firestick-loader\partitions-dump"
-%shell% rm %sdcard%/userdata.img
+echo.
+echo.
+echo.
+echo Dumping userdata Partition....
+echo.
+echo.
 
-%dd% if=/dev/block/platform/sdhci.1/by-name/vc-firmware of=/sdcard/vc-firmware.img"
-%pull% %sdcard%/vc-firmware.img "%temp%\firestick-loader\partitions-dump"
+::%dd% if=%blockBase%/userdata of=/%sdcard%/userdata.img"
+::%pull% %sdcard%/userdata.img %dumpFolder%
+::%shell% rm %sdcard%/userdata.img
+
+echo.
+echo.
+echo.
+echo Dumping vc-firmware Partition....
+echo.
+echo.
+
+%dd% if=%blockBase%/vc-firmware of=/%sdcard%/vc-firmware.img"
+%pull% %sdcard%/vc-firmware.img %dumpFolder%
 %shell% rm %sdcard%/vc-firmware.img
+
+echo.
+echo.
+echo.
+echo Opening Dump Folder....
+echo.
+echo.
 
 
 color 0a
 
 
-start "" explorer "%temp%\firestick-loader\partitions-dump"
+start "" explorer %dumpFolder%
 
 
 :end
