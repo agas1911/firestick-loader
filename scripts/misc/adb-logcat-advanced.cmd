73490d32d132830f285edf61665f00570be6ed9a
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-17 23:43:27 -0500

    Updated Text

--- scripts/misc/adb-logcat-advanced.cmd
+++ scripts/misc/adb-logcat-advanced.cmd
@@ -5,9 +5,10 @@ color 0b
 set adb="..\..\bin\adb.exe"
 set shell=%adb% shell
 
+set text=none
 
 cls
-echo Set GREP Text (if any) and Press ENTER:
+echo Set GREP Text and Press ENTER:
 echo.
 echo.
 echo ** For Multiple Entries Use "text1 | grep text2" Format (NO QUOTES) **
@@ -19,11 +20,8 @@ echo.
 set /p text=
 
 
-:loop
-
 %shell% "logcat v | grep %text%"
 
-goto loop
 
 
 :end
