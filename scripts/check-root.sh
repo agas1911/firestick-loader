511633eacc994f93fead285c592f95097379d14d
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-18 06:30:15 -0500

    Cleaned Up checkRoot Script

--- scripts/check-root.sh
+++ scripts/check-root.sh
@@ -10,31 +10,13 @@
 
 
 checkRootStatus(){
-
-	userPriv="none"
-	
-	#isRoot=$(id | grep root)
 	
 	if [ -f /system/xbin/su ];
 	then
-		isRoot="1"
-	else
-		isRoot="0"
-	fi
-
-	case "$isRoot" in
-
-		"0")
-		userPriv="none"
-		echo "0">/sdcard/rootAccess.txt
-		;;
-
-		"1")
-		userPriv="root"
 		echo "1">/sdcard/rootAccess.txt
-		;;
-
-	esac
+	else
+		echo "0">/sdcard/rootAccess.txt
+	fi
 	
 }
 	
