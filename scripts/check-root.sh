1ca1e2a250f713cc07ebcb0b4f9ff0f977d209de
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-18 06:28:29 -0500

    Fixed checkRoot Function To Actually Work

--- scripts/check-root.sh
+++ scripts/check-root.sh
@@ -12,17 +12,24 @@
 checkRootStatus(){
 
 	userPriv="none"
-
-	isRoot=$(id | grep root)
+	
+	#isRoot=$(id | grep root)
+	
+	if [ -f /system/xbin/su ];
+	then
+		isRoot="1"
+	else
+		isRoot="0"
+	fi
 
 	case "$isRoot" in
 
-		"")
+		"0")
 		userPriv="none"
 		echo "0">/sdcard/rootAccess.txt
 		;;
 
-		"root")
+		"1")
 		userPriv="root"
 		echo "1">/sdcard/rootAccess.txt
 		;;
